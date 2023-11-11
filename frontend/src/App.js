f1465274e3e1068883430808f3be1cbc1ea28e91
Daniel Pham <danielpham357@gmail.com>
2023-11-10 16:48:11 -0800

    Restore: Backend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../src/index.css";
 import Dashboard from "./containers/Dashboard";
 
 function App() {
   // api call to backend to get the hello world message
-  // const [message, setMessage] = useState("")
-  // useEffect(() => {
-  // fetch('/hello')
-  //   .then(res => res.text())
-  //   .then(res => setMessage(res));
-  // } , []);
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => res.text())
+      .then((res) => setMessage(res));
+  }, []);
 
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold underline">{message}</h1> */}
+      <h1 className="text-3xl font-bold underline">{message}</h1>
       <Dashboard />
     </div>
   );
