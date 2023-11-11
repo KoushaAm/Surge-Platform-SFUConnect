03903f0a3f9678b30be33ff632de483d59283de0
Daniel Pham <danielpham357@gmail.com>
2023-11-10 16:24:14 -0800

    Add: Page containers

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Home content</h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/containers/Signup.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Signup content</h1>
+    </>
+  );
+};
+
+export default Home;
