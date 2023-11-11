51d53f4c0b6f0d5d2b5b21a8f069f2a2807ce5fd
Daniel Pham <danielpham357@gmail.com>
2023-11-10 16:32:12 -0800

    Update: npm start script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import { useState, useEffect } from 'react';
-import '../src/index.css'
+// import { useState, useEffect } from "react";
+import "../src/index.css";
+import Dashboard from "./containers/Dashboard";
 
 function App() {
-
-  const [message, setMessage] = useState("")
-  // api call to backend to get the hellow world message
-  useEffect(() => {
-  fetch('/hello')
-    .then(res => res.text())
-    .then(res => setMessage(res));
-  } , []);
-
+  // api call to backend to get the hello world message
+  // const [message, setMessage] = useState("")
+  // useEffect(() => {
+  // fetch('/hello')
+  //   .then(res => res.text())
+  //   .then(res => setMessage(res));
+  // } , []);
 
   return (
-    
     <div>
-      
-      <h1 className = "text-3xl font-bold underline">{message}</h1>
+      {/* <h1 className="text-3xl font-bold underline">{message}</h1> */}
+      <Dashboard />
     </div>
-
-
   );
 }
 
