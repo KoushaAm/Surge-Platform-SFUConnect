55a21bc6fd1125fe0cdd224e2e2475e38123f84d
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-11-13 10:39:16 -0800

    database connected

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,44 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const {MongoClient} = require('mongodb');
 
+async function main() {
+  const uri = "mongodb+srv://KoushaAm:ksh.orangesurge@orangedatabase.voevsap.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
 
+  try {
+    await client.connect();
+    
+    listDatabases(client);
+    readDataFromMongoD(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    setTimeout(() => {client.close()}, 10000)
+  }
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+main().catch(console.error);
+
+async function readDataFromMongoD(client) {
+  const collection = client.db("Clubs").collection("clubs_info");
+  const cursor = collection.find({});
+  await cursor.forEach(console.dir);
+}
 
 // send hello world
 app.get('/hello', (req, res) => {
   res.send('This is a message from the server!');
 });
 
-const port = 4000;
+const port = 8000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
