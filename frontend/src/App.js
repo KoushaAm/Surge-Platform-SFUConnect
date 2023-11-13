2634f81c1978d8381b51d71d44869c146f9a7921
Daniel Pham <danielpham357@gmail.com>
2023-11-13 14:26:09 -0800

    Add: Routes and path to navigate dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import "../src/index.css";
+
+import AppLayout from "./components/layout/AppLayout";
+
 import Dashboard from "./containers/Dashboard";
 
 function App() {
   // api call to backend to get the hello world message
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch("/hello")
-      .then((res) => res.text())
-      .then((res) => setMessage(res));
-  }, []);
+  // const [message, setMessage] = useState("");
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((res) => res.text())
+  //     .then((res) => setMessage(res));
+  // }, []);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">{message}</h1>
-      <Dashboard />
+      {/* <h1 className="text-3xl font-bold underline">{message}</h1> */}
+      <Routes>
+        <Route path="/app" element={<AppLayout />}>
+          <Route path="/app/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
