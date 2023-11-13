413fd4891bbe14451a1d039d4196b068a9957436
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-11-12 16:09:01 -0800

    reformatter file for importing database

---
+++ backend/clubs_extraction/clubs_mongodb.py
@@ -0,0 +1,28 @@
+import json
+
+# this class reformats the clubs.json to be comaptible to MongoDB's template
+def convert_to_mongodb_format(input_filename, output_filename):
+
+    input_filename = './backend/clubs.json'
+    output_filename = './backend/mongodb_clubs.json'
+
+    with open(input_filename, 'r') as file:
+        data = json.load(file)
+
+    mongodb_data = []
+    for club_name, club_data in data.items():
+        mongodb_entry = {
+            "_id": club_data["id"],
+            "title": club_data["title"],
+            "description": club_data["description"],
+            "category": club_data["category"],
+            "info_url": club_data["info_url"],
+            "logo_url": club_data["logo_url"]
+        }
+        mongodb_data.append(mongodb_entry)
+
+    with open(output_filename, 'w') as output_file:
+        json.dump(mongodb_data, output_file, indent=2)
+
+
+convert_to_mongodb_format('./clubs.json', 'mongodb_clubs.json')
