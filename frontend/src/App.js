f8e21ab6b107964156f6fd8101865a874761974c
Daniel Pham <danielpham357@gmail.com>
2023-11-13 17:24:06 -0800

    Restore: Test message from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "../src/index.css";
 
@@ -6,33 +6,41 @@ import PageLayout from "./components/layout/PageLayout";
 import AppLayout from "./components/layout/AppLayout";
 
 import Home from "./containers/Home";
+import About from "./containers/About";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import Error from "./containers/Error";
+import UserProfile from "./containers/UserProfile";
 import Dashboard from "./containers/Dashboard";
+import MyClubs from "./containers/MyClubs";
+import ExploreClubs from "./containers/ExploreClubs";
 
 function App() {
   // api call to backend to get the hello world message
-  // const [message, setMessage] = useState("");
-  // useEffect(() => {
-  //   fetch("/hello")
-  //     .then((res) => res.text())
-  //     .then((res) => setMessage(res));
-  // }, []);
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => res.text())
+      .then((res) => setMessage(res));
+  }, []);
 
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold underline">{message}</h1> */}
+      <h1 className="text-3xl font-bold underline">{message}</h1>
       <Routes>
         <Route element={<PageLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<Error />} />
         </Route>
 
         <Route path="/app" element={<AppLayout />}>
+          <Route path="/app/profile" element={<UserProfile />} />
           <Route path="/app/dashboard" element={<Dashboard />} />
+          <Route path="/app/myclubs" element={<MyClubs />} />
+          <Route path="/app/exploreclubs" element={<ExploreClubs />} />
         </Route>
       </Routes>
     </div>
