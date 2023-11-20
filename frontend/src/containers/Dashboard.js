2dbf27ad20f22b277dd6e292cb99cb62166f013b
Daniel Pham <danielpham357@gmail.com>
2023-11-20 00:46:16 -0800

    Update: Add categories for testing

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -2,16 +2,23 @@ import { useExploreClubsContext } from "../context/ExploreClubsContext";
 import { useEffect } from "react";
 
 const Dashboard = () => {
-  const { getAllClubsHandler } = useExploreClubsContext();
+  const { getAllClubsHandler, getAllCategoriesHandler } =
+    useExploreClubsContext();
 
   useEffect(() => {
     // Fetch all clubs data from backend here
-    // replace the test_all_clubs_data
+    // replace the test_all_clubs_data and test_all_categories_data with frontend API calls
     const test_all_clubs_data = {
       name: "Surge",
     };
+
+    const test_all_categories_data = {
+      category: "Projects",
+    };
+
     getAllClubsHandler(test_all_clubs_data);
-  }, [getAllClubsHandler]);
+    getAllCategoriesHandler(test_all_categories_data);
+  }, [getAllClubsHandler, getAllCategoriesHandler]);
 
   return (
     <div>
