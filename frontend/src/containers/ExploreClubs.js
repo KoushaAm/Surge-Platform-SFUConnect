a8a1d5c8e00196e5ee9a00d85c92fb4dfd337676
Daniel Pham <danielpham357@gmail.com>
2023-11-20 00:34:03 -0800

    Add: Test display data on Explore Clubs tab

--- frontend/src/containers/ExploreClubs.js
+++ frontend/src/containers/ExploreClubs.js
@@ -1,9 +1,14 @@
+import { useExploreClubsContext } from "../context/ExploreClubsContext";
+
 const ExploreClubs = () => {
-    return (
-      <div>
-        <h1> All clubs content </h1>
-      </div>
-    );
-  };
-  
-  export default ExploreClubs;
+  const { clubs } = useExploreClubsContext();
+
+  return (
+    <div>
+      <h1> All clubs content </h1>
+      {console.log(clubs)}
+    </div>
+  );
+};
+
+export default ExploreClubs;
