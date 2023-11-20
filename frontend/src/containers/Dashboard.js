ac0e3dbc61fbdf184e44d8ce1d973aa622a688e7
Daniel Pham <danielpham357@gmail.com>
2023-11-20 00:33:30 -0800

    Add: Able to fetch data when user log in

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,4 +1,18 @@
+import { useExploreClubsContext } from "../context/ExploreClubsContext";
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const { getAllClubsHandler } = useExploreClubsContext();
+
+  useEffect(() => {
+    // Fetch all clubs data from backend here
+    // replace the test_all_clubs_data
+    const test_all_clubs_data = {
+      name: "Surge",
+    };
+    getAllClubsHandler(test_all_clubs_data);
+  }, [getAllClubsHandler]);
+
   return (
     <div>
       <h1> dashboard content </h1>
