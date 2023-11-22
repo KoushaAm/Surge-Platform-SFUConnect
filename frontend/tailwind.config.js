c144e0d73fa6583b39de2e0fdd117489db7a7874
kylezhao101 <kylezhao101@gmail.com>
2023-11-22 13:51:35 -0800

    import google fonts and setup custom tailwind color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    colors: {
+      'custom-dark-gray':'#AAAAAA',
+      'custom-orange-primary':'#FFA25F',
+    },
+    extend: {
+      fontFamily: {
+        'rubik': ['Rubik'],
+        'karla': ['Karla']
+      },
+    },
   },
   plugins: [],
 }
