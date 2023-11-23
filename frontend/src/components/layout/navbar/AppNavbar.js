9ca16f5f04ba9a9740303801e40ab4c5766b8129
kylezhao101 <kylezhao101@gmail.com>
2023-11-22 16:14:48 -0800

    display svg icon and style navbar element

--- frontend/src/components/layout/navbar/AppNavbar.js
+++ frontend/src/components/layout/navbar/AppNavbar.js
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import myClubs from "../../../assets/icons/myClubs.svg";
+import allClubs from "../../../assets/icons/allClubs.svg";
 
 const Navbar = () => {
   const links = [
@@ -13,26 +15,26 @@ const Navbar = () => {
     {
       text: "My Clubs",
       path: "/app/myclubs",
+      icon: <img src={myClubs} alt="My Clubs" className="mr-4"/>,
     },
     {
-      text: "Explore Clubs",
+      text: "All Clubs",
       path: "/app/exploreclubs",
+      icon: <img src={allClubs} alt="My Clubs" className="mr-4"/>,
     },
   ];
 
   return (
     <nav>
       <ul>
-        {links.map((link) => {
-          return (
-            <li
-              key={link.text}
-              style={{ color: "blue", textDecorationLine: "underline" }}
-            >
-              <NavLink to={link.path}>{link.text}</NavLink>
-            </li>
-          );
-        })}
+        {links.map((link) => (
+          <li key={link.text} className="flex items-center p-3">
+            <NavLink to={link.path} className="font-rubik text-xl text-custom-dark-gray flex items-center">
+              {link.icon}
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,14 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    colors: {
-      'custom-dark-gray':'#AAAAAA',
-      'custom-orange-primary':'#FFA25F',
-    },
     extend: {
       fontFamily: {
         'rubik': ['Rubik'],
         'karla': ['Karla']
+      },
+      colors: {
+        'custom-dark-gray':'#AAAAAA',
+        'custom-orange-primary':'#FFA25F',
       },
     },
   },
