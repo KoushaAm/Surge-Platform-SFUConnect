514d8cebe776dd52e9d01119c35b4739e8a6c348
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2024-03-01 16:05:45 -0800

    documentation

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
+// Backend for myConnect Frontend
+// author: Kousha Amouzesh
+// This is a  express server that listens on port 8000
+// It has 3 endpoints:
+// 1. GET /clubs - returns all the clubs in the database
+// 2. GET /clubs/category - returns all the clubs in the database that match the category in the query parameter
+// 3. GET /clubs/search - returns all the clubs in the database that match the search term in the query parameter
+
+// TODO: 
+// 1. Add a POST /signup and GET /login endpoint to handle user authentication
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { MongoClient } = require("mongodb");
 
+// TODO: confidentialize the URI
 const URI = 'mongodb+srv://demo:surge2023@orangedatabase.voevsap.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(bodyParser.json());
@@ -102,15 +114,16 @@ app.get('/clubs', async (req, res) => {
 
     const result = await listClubs(client);
     res.json(result);
-    console.log("/clubs called")
+    // console.log("/clubs called")
     return res;
+
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-
+// collects the clubs listings from the mongoDB cluster and returns it as an array of json
 async function listClubs() {
   const collection = client.db("Clubs").collection("clubs_info");
   const result = await collection.find({}).toArray();
@@ -130,7 +143,7 @@ process.on('SIGINT', async () => {
   }
 });
 
-
+// tests the connection to the database and lists the databases
 async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
 
